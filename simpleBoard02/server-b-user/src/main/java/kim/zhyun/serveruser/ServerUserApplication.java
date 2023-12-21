ae6357d3751ec784f7642bc72c0e589466c72ac1
zhyun <gimwlgus@gmail.com>
2023-12-21 19:24:15 +0900

    chore : project init (#12)
    프로젝트 뼈대 구축
    
    issue key : SB2-4

---
+++ simpleBoard02/server-b-user/src/main/java/kim/zhyun/serveruser/ServerUserApplication.java
@@ -0,0 +1,11 @@
+package kim.zhyun.serveruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerUserApplication.class, args);
+    }
+}
---
+++ simpleBoard02/server-c-article/src/main/java/kim/zhyun/serverarticle/ServerArticleApplication.java
@@ -0,0 +1,11 @@
+package kim.zhyun.serverarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerArticleApplication.class, args);
+    }
+}
