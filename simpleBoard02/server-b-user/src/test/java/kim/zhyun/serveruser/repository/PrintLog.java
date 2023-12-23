6b6ff40d10182f4d9720ff2c107a39f0d5e0ca0f
zhyunk <gimwlgus@gmail.com>
2023-12-24 00:02:42 +0900

    chore : test setting
    issue key : SB2-6

---
+++ simpleBoard02/server-b-user/src/test/java/kim/zhyun/serveruser/repository/PrintLog.java
@@ -0,0 +1,38 @@
+package kim.zhyun.serveruser.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+@Slf4j
+public class PrintLog <Repository> {
+    Repository repository;
+    
+    public PrintLog(Repository repository) {
+        this.repository = repository;
+    }
+    
+    public <T> void print(List<T> data) {
+        data.forEach(item -> log.info("\t\t{}", item));
+    }
+    
+    @AfterEach
+    @BeforeEach
+    public void printLog() {
+        log.info("💁 All Data Logging ------------------------------------------------------------------------------------------------------------------------------------------------------------┐");
+        
+        if (repository instanceof RoleRepository)
+            print(((RoleRepository) repository).findAll());
+        
+        if (repository instanceof AuthRepository)
+            print(((AuthRepository) repository).findAll());
+        
+        if (repository instanceof UserRepository)
+            print(((UserRepository) repository).findAll());
+        
+        log.info("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------┘");
+    }
+    
+}
