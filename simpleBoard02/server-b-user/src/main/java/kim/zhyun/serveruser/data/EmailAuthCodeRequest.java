cdb08a4ac9bfe43c22685df0d25f67afb81c1b5c
zhyunk <gimwlgus@gmail.com>
2023-12-28 16:42:51 +0900

    test : 인증 코드 메일 발송 controller test
    EmailAuthCodeRequest.java
    - 객체 비교시 필드의 값만으로 비교하기 위해 equals() 재정의
    
    CheckControllerTest.java
    - 기존에 Session id 관련 상수를 전역 상수로 만들어서 사용하던 것을 MockHttpSession() 객체에서 session id를 가져와 사용하도록 변경
    - 인증 코드 메일 발송 관련 테스트 추가
    
    issue key : SB2-18

--- simpleBoard02/server-b-user/src/main/java/kim/zhyun/serveruser/data/EmailAuthCodeRequest.java
+++ simpleBoard02/server-b-user/src/main/java/kim/zhyun/serveruser/data/EmailAuthCodeRequest.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @NoArgsConstructor
 @AllArgsConstructor(staticName = "of")
 @Getter
@@ -15,4 +17,13 @@ public class EmailAuthCodeRequest {
     @Email(message = "올바른 이메일 주소를 입력해주세요.", regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$")
     private String email;
     
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        
+        EmailAuthCodeRequest that = (EmailAuthCodeRequest) obj;
+        
+        return Objects.equals(email, that.email);
+    }
 }
--- simpleBoard02/server-b-user/src/test/java/kim/zhyun/serveruser/controller/CheckControllerTest.java
+++ simpleBoard02/server-b-user/src/test/java/kim/zhyun/serveruser/controller/CheckControllerTest.java
@@ -1,5 +1,7 @@
 package kim.zhyun.serveruser.controller;
 
+import kim.zhyun.serveruser.advice.MailAuthException;
+import kim.zhyun.serveruser.data.EmailAuthCodeRequest;
 import kim.zhyun.serveruser.repository.container.RedisTestContainer;
 import kim.zhyun.serveruser.service.SignUpService;
 import lombok.extern.slf4j.Slf4j;
@@ -10,12 +12,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
+import static kim.zhyun.serveruser.data.message.ExceptionMessage.REQUIRED_REQUEST_BODY;
+import static kim.zhyun.serveruser.data.message.ExceptionMessage.REQUIRE_MAIL_DUPLICATE_CHECK;
 import static kim.zhyun.serveruser.data.message.ResponseMessage.*;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,12 +37,16 @@ class CheckControllerTest {
     private final String NICKNAME = "얼거스";
     private final String EMAIL = "gimwlgus@gmail.com";
     
+    private final String EMAIL_VALID_EXCEPTION_MESSAGE = "올바른 이메일 주소를 입력해주세요.";
+    
     @MockBean
     private SignUpService signupService;
     
     private final MockMvc mvc;
+    private final ObjectMapper mapper;
     public CheckControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
+        this.mapper = new ObjectMapper();
     }
     
     @DisplayName("중복 확인 - 빈 값 입력")
@@ -53,31 +65,33 @@ class CheckControllerTest {
     @DisplayName("닉네임 중복 확인 - 사용 가능")
     @Test
     void duplicate_check_nickname() throws Exception {
-        when(signupService.availableNickname(anyString(), anyString())).thenReturn(true);
+        MockHttpSession session = new MockHttpSession();
+        String sessionId = session.getId();
+        when(signupService.availableNickname(NICKNAME, sessionId)).thenReturn(true);
         
-        mvc.perform(get("/check").param("nickname", NICKNAME))
+        mvc.perform(get("/check").param("nickname", NICKNAME).session(session))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("status").value(true))
                 .andExpect(jsonPath("message").value(SIGN_UP_AVAILABLE_NICKNAME.getMessage()))
                 .andDo(print());
-        
-        verify(signupService, times(0)).availableEmail(anyString(), anyString());
-        verify(signupService, times(1)).availableNickname(anyString(), anyString());
     }
     
     @DisplayName("닉네임 중복 확인 - 사용 불가")
     @Test
     void duplicate_check_nickname_using() throws Exception {
-        when(signupService.availableNickname(anyString(), anyString())).thenReturn(false);
+        MockHttpSession session = new MockHttpSession();
+        String sessionId = session.getId();
+        when(signupService.availableNickname(NICKNAME, sessionId)).thenReturn(false);
         
-        mvc.perform(get("/check").param("nickname", NICKNAME))
+        mvc.perform(get("/check").param("nickname", NICKNAME)
+                        .session(session))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("status").value(false))
                 .andExpect(jsonPath("message").value(SIGN_UP_UNAVAILABLE_NICKNAME.getMessage()))
                 .andDo(print());
         
-        verify(signupService, times(0)).availableEmail(anyString(), anyString());
-        verify(signupService, times(1)).availableNickname(anyString(), anyString());
+        verify(signupService, times(0)).availableEmail(EMAIL, sessionId);
+        verify(signupService, times(1)).availableNickname(NICKNAME, sessionId);
     }
     
     @DisplayName("닉네임 중복 확인 - 유효한 형식이 아님")
@@ -133,7 +147,7 @@ class CheckControllerTest {
                 .andExpect(jsonPath("$.status").value(false))
                 .andExpect(jsonPath("$.message").value(VALID_EXCEPTION.getMessage()))
                 .andExpect(jsonPath("$.result.[0].field").value("email"))
-                .andExpect(jsonPath("$.result.[0].message").value("올바른 이메일 주소를 입력해주세요."))
+                .andExpect(jsonPath("$.result.[0].message").value(EMAIL_VALID_EXCEPTION_MESSAGE))
                 .andDo(print());
         
         verify(signupService, times(0)).availableEmail(anyString(), anyString());
@@ -148,10 +162,113 @@ class CheckControllerTest {
                 .andExpect(jsonPath("$.status").value(false))
                 .andExpect(jsonPath("$.message").value(VALID_EXCEPTION.getMessage()))
                 .andExpect(jsonPath("$.result.[0].field").value("email"))
-                .andExpect(jsonPath("$.result.[0].message").value("올바른 이메일 주소를 입력해주세요."))
+                .andExpect(jsonPath("$.result.[0].message").value(EMAIL_VALID_EXCEPTION_MESSAGE))
                 .andDo(print());
         
         verify(signupService, times(0)).availableEmail(anyString(), anyString());
         verify(signupService, times(0)).availableNickname(anyString(), anyString());
+    }
+    
+    @DisplayName("인증 코드 메일 발송 - request body is empty")
+    @Test
+    void send_email_fail_not_input_email() throws Exception {
+        // when-then
+        mvc.perform(post("/check/auth")
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(false))
+                .andExpect(jsonPath("$.message").value(REQUIRED_REQUEST_BODY.getMessage()))
+                .andDo(print());
+        
+        verify(signupService, times(0)).sendEmailAuthCode("", null);
+    }
+    
+    @DisplayName("인증 코드 메일 발송 - 이메일 입력 안됨")
+    @Test
+    void send_email_fail_email_valid_exception_null() throws Exception {
+        // given
+        EmailAuthCodeRequest given = EmailAuthCodeRequest.of(null);
+        
+        // when-then
+        mvc.perform(post("/check/auth")
+                        .contentType(APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(given)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(false))
+                .andExpect(jsonPath("$.message").value(VALID_EXCEPTION.getMessage()))
+                .andExpect(jsonPath("$.result.[0].field").value("email"))
+                .andExpect(jsonPath("$.result.[0].message").value(EMAIL_VALID_EXCEPTION_MESSAGE))
+                .andDo(print());
+        
+        verify(signupService, times(0)).sendEmailAuthCode("", given);
+    }
+    
+    @DisplayName("인증 코드 메일 발송 - 이메일 형식 오류")
+    @Test
+    void send_email_fail_email_valid_exception() throws Exception {
+        // given
+        EmailAuthCodeRequest given = EmailAuthCodeRequest.of("test@asd");
+        
+        // when-then
+        mvc.perform(post("/check/auth")
+                        .contentType(APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(given)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(false))
+                .andExpect(jsonPath("$.message").value(VALID_EXCEPTION.getMessage()))
+                .andExpect(jsonPath("$.result.[0].field").value("email"))
+                .andExpect(jsonPath("$.result.[0].message").value(EMAIL_VALID_EXCEPTION_MESSAGE))
+                .andDo(print());
+        
+        verify(signupService, times(0)).sendEmailAuthCode("", given);
+    }
+    
+    @DisplayName("인증 코드 메일 발송 - fail : 이메일 중복검사 안함")
+    @Test
+    void send_email_fail_email_duplicate_check_pass() throws Exception {
+        // given
+        MockHttpSession session = new MockHttpSession();
+        String sessionId = session.getId();
+        
+        EmailAuthCodeRequest given = EmailAuthCodeRequest.of(EMAIL);
+        
+        doThrow(new MailAuthException(REQUIRE_MAIL_DUPLICATE_CHECK))
+                .when(signupService).sendEmailAuthCode(sessionId, given);
+        
+        // when-then
+        mvc.perform(post("/check/auth")
+                        .contentType(APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(given))
+                        .session(session))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(false))
+                .andExpect(jsonPath("$.message").value(REQUIRE_MAIL_DUPLICATE_CHECK.getMessage()))
+                .andDo(print());
+        
+        verify(signupService, times(1)).sendEmailAuthCode(sessionId, given);
+    }
+    
+    @DisplayName("인증 코드 메일 발송 - success")
+    @Test
+    void send_email_success() throws Exception {
+        // given
+        MockHttpSession session = new MockHttpSession();
+        String sessionId = session.getId();
+        
+        EmailAuthCodeRequest given = EmailAuthCodeRequest.of(EMAIL);
+        
+        doNothing().when(signupService).sendEmailAuthCode(sessionId, given);
+        
+        // when-then
+        mvc.perform(post("/check/auth")
+                        .contentType(APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(given))
+                        .session(session))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(true))
+                .andExpect(jsonPath("$.message").value(SEND_EMAIL_AUTH_CODE.getMessage()))
+                .andDo(print());
+        
+        verify(signupService, times(1)).sendEmailAuthCode(sessionId, given);
     }
 }
