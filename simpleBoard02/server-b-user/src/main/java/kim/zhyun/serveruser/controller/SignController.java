0a6ec36addd8bcc810be7754b940d1415aa0f628
zhyunk <gimwlgus@gmail.com>
2023-12-30 09:29:17 +0900

    fix : sign controller - signUp()
    매개변수에 @Valid @RquestBody가 안보여서 붙여줌
    
    issue key : SB2-10

--- simpleBoard02/server-b-user/src/main/java/kim/zhyun/serveruser/controller/SignController.java
+++ simpleBoard02/server-b-user/src/main/java/kim/zhyun/serveruser/controller/SignController.java
@@ -3,15 +3,13 @@ package kim.zhyun.serveruser.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import kim.zhyun.serveruser.data.ApiResponse;
 import kim.zhyun.serveruser.data.SignupRequest;
 import kim.zhyun.serveruser.service.SignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static kim.zhyun.serveruser.data.message.ResponseMessage.SUCCESS_FORMAT_SIGN_UP;
 
@@ -24,7 +22,8 @@ public class SignController {
     
     @Operation(summary = "회원가입")
     @PostMapping("/sign-up")
-    public ResponseEntity<ApiResponse<Void>> signUp(HttpServletRequest request, SignupRequest signupRequest) {
+    public ResponseEntity<ApiResponse<Void>> signUp(HttpServletRequest request,
+                                                    @Valid @RequestBody SignupRequest signupRequest) {
         signUpService.saveMember(request.getSession().getId(), signupRequest);
 
         return ResponseEntity.ok(ApiResponse.<Void>builder()
