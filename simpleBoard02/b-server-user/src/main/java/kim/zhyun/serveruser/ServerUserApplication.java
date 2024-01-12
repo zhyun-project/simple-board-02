00849778c4c48728c7a5b04468b4eba8b4e2cb9e
zhyunk <gimwlgus@gmail.com>
2024-01-12 13:45:30 +0900

    chore : [ user-server, article-server ] discovery 등록
    issue key : SB2-9

--- simpleBoard02/b-server-user/src/main/java/kim/zhyun/serveruser/ServerUserApplication.java
+++ simpleBoard02/b-server-user/src/main/java/kim/zhyun/serveruser/ServerUserApplication.java
@@ -2,8 +2,10 @@ package kim.zhyun.serveruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"kim.zhyun.serveruser", "kim.zhyun.jwt"})
 public class ServerUserApplication {
--- simpleBoard02/c-server-article/src/main/java/kim/zhyun/serverarticle/ServerArticleApplication.java
+++ simpleBoard02/c-server-article/src/main/java/kim/zhyun/serverarticle/ServerArticleApplication.java
@@ -2,7 +2,9 @@ package kim.zhyun.serverarticle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"kim.zhyun.serverarticle", "kim.zhyun.jwt"})
 public class ServerArticleApplication {
     public static void main(String[] args) {
