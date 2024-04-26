22d6db950aaa0350e6d8e14e055354f58cb00581
zhyunk <gimwlgus@gmail.com>
2024-04-27 04:22:35 +0900

    fix:  [user]  /error 요청 - security permitAll 등록

--- simpleBoard02/b-server-user/src/main/java/kim/zhyun/serveruser/config/SecurityConfig.java
+++ simpleBoard02/b-server-user/src/main/java/kim/zhyun/serveruser/config/SecurityConfig.java
@@ -43,6 +43,8 @@ public class SecurityConfig {
                         "/swagger-ui/**",
                         "/v3/api-docs/**",
                         
+                        "/error/**",
+                        
                         "/sign-up/**",
                         "/check/**",
                         "/login/**"
