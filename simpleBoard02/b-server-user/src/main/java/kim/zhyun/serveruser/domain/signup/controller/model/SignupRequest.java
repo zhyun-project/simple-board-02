a35b9d5ebe81999e379e130b2bac11038e3092ce
zhyunk <gimwlgus@gmail.com>
2024-05-12 15:17:34 +0900

    fix:  [user] sign up request
    valid 어노테이션 고침
    
    issue key : #81

--- simpleBoard02/b-server-user/src/main/java/kim/zhyun/serveruser/domain/signup/controller/model/SignupRequest.java
+++ simpleBoard02/b-server-user/src/main/java/kim/zhyun/serveruser/domain/signup/controller/model/SignupRequest.java
@@ -2,6 +2,8 @@ package kim.zhyun.serveruser.domain.signup.controller.model;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import kim.zhyun.serveruser.common.annotation.Email;
+import kim.zhyun.serveruser.common.annotation.Nickname;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,8 +17,8 @@ import static kim.zhyun.serveruser.common.message.ExceptionMessage.EXCEPTION_VAL
 @NoArgsConstructor
 public class SignupRequest {
     
-    @NotNull private String email;
-    @NotNull private String nickname;
+    @Email private String email;
+    @Nickname private String nickname;
     
     @NotNull @Size(min = 4, message = EXCEPTION_VALID_PASSWORD_FORMAT)
     private String password;
