e089d5eb6c79f362afbcc4b92345c13a79b2df79
zhyunk <gimwlgus@gmail.com>
2024-05-13 22:49:59 +0900

    test: [user] check api controller test 재작성
    webMvc -> mock
    
    issue : #81

--- simpleBoard02/b-server-user/src/test/java/kim/zhyun/serveruser/domain/signup/controller/CheckApiControllerTest.java
+++ simpleBoard02/b-server-user/src/test/java/kim/zhyun/serveruser/domain/signup/controller/CheckApiControllerTest.java
@@ -1,57 +1,63 @@
 package kim.zhyun.serveruser.domain.signup.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import kim.zhyun.serveruser.container.RedisTestContainer;
+import kim.zhyun.jwt.common.model.ApiResponse;
+import kim.zhyun.jwt.exception.ApiException;
+import kim.zhyun.serveruser.common.message.ExceptionMessage;
+import kim.zhyun.serveruser.common.message.ResponseMessage;
+import kim.zhyun.serveruser.domain.signup.business.SignUpBusiness;
 import kim.zhyun.serveruser.domain.signup.controller.model.EmailAuthCodeRequest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 
-@Order(0)
-@ExtendWith(RedisTestContainer.class)
-@AutoConfigureMockMvc
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class CheckApiControllerTest {
     
-    @Autowired MockMvc mvc;
-    @Autowired ObjectMapper objectMapper;
+    @InjectMocks CheckApiController checkApiController;
+    @Mock SignUpBusiness signUpBusiness;
     
     
     
     @DisplayName("중복 확인 실패 - 이메일 = null, 닉네임 = null")
     @Test
-    void duplicateCheck_fail() throws Exception {
-        MockHttpSession session = new MockHttpSession();
+    void duplicateCheck_fail() {
+        // given
+        String email = null;
+        String nickname = null;
         
-        mvc.perform(
-                MockMvcRequestBuilders.get("/check")
-                        .session(session)
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value("값을 입력해주세요."))
-                .andDo(print());
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
         
+        
+        // when
+        ResponseEntity<ApiResponse<Void>> responseEntity = checkApiController.duplicateCheck(request, email, nickname);
+        
+        
+        // then
+        assertEquals(responseEntity.getStatusCode(),        HttpStatusCode.valueOf(HttpStatus.OK.value()));
+        assertEquals(responseEntity.getBody().getStatus(),  false);
+        assertEquals(responseEntity.getBody().getMessage(), ResponseMessage.RESPONSE_SIGN_UP_CHECK_VALUE_IS_EMPTY);
     }
+    
     
     @DisplayName("이메일 중복 확인 성공")
     @ParameterizedTest
-    @ValueSource(strings = {
+    @CsvSource({
             "email.com@address.mail",
             "e-mail@email.mail",
             "e+mail@email.mail",
@@ -64,19 +70,28 @@ class CheckApiControllerTest {
             "1e+mail@email.mail.ail",
             "1e_mail@email.mail.ail",
             "1e.mail@email.mail.ail",
-            "1email@email.mail.ail",
+            "1email@email.mail.ail"
     })
-    void duplicateCheck_email_success(String email) throws Exception {
-        MockHttpSession session = new MockHttpSession();
+    void duplicateCheck_email_success(String email) {
+        // given
+        String nickname = null;
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+
+        String responseMessage = ResponseMessage.RESPONSE_SIGN_UP_AVAILABLE_EMAIL;
         
-        mvc.perform(
-                MockMvcRequestBuilders.get("/check")
-                        .param("email", email)
-                        .session(session)
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value("사용 가능한 이메일입니다. 이메일 인증을 진행해주세요."))
-                .andDo(print());
+        given(signUpBusiness.emailDuplicateCheck(email, request.getSession().getId())).willReturn(responseMessage);
+        
+        
+        // when
+        ResponseEntity<ApiResponse<Void>> responseEntity = checkApiController.duplicateCheck(request, email, nickname);
+        
+        
+        // then
+        assertEquals(responseEntity.getStatusCode(),        HttpStatusCode.valueOf(HttpStatus.OK.value()));
+        assertEquals(responseEntity.getBody().getStatus(),  true);
+        assertEquals(responseEntity.getBody().getMessage(), responseMessage);
     }
     
     @DisplayName("이메일 중복 확인 실패 - 규격 안맞음")
@@ -102,17 +117,26 @@ class CheckApiControllerTest {
             "e　mail@wemilamasd.com",
             "email@wemila@masd.com",
     })
-    void duplicateCheck_email_fail(String email) throws Exception {
-        MockHttpSession session = new MockHttpSession();
+    void duplicateCheck_email_fail(String email) {
+        // given
+        String nickname = null;
         
-        mvc.perform(
-                MockMvcRequestBuilders.get("/check")
-                        .param("email", email)
-                        .session(session)
-                )
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value("입력 값이 올바르지 않습니다."))
-                .andDo(print());
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        String responseMessage = ResponseMessage.RESPONSE_SIGN_UP_UNAVAILABLE_EMAIL;
+        
+        given(signUpBusiness.emailDuplicateCheck(email, request.getSession().getId())).willReturn(responseMessage);
+        
+        
+        // when
+        ResponseEntity<ApiResponse<Void>> responseEntity = checkApiController.duplicateCheck(request, email, nickname);
+        
+        
+        // then
+        assertEquals(responseEntity.getStatusCode(),        HttpStatusCode.valueOf(HttpStatus.OK.value()));
+        assertEquals(responseEntity.getBody().getStatus(),  false);
+        assertEquals(responseEntity.getBody().getMessage(), responseMessage);
     }
     
     
@@ -131,18 +155,26 @@ class CheckApiControllerTest {
             "일이🦆사오육",
             "🙆🙆🙆🙆🙆🙆",
     })
-    void duplicateCheck_nickname_success(String nickname) throws Exception {
-        MockHttpSession session = new MockHttpSession();
+    void duplicateCheck_nickname_success(String nickname) {
+        // given
+        String email = null;
         
-        mvc.perform(
-                MockMvcRequestBuilders.get("/check")
-                        .param("nickname", nickname)
-                        .session(session)
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status").value(true))
-                .andExpect(jsonPath("$.message").value("사용 가능한 닉네임입니다."))
-                .andDo(print());
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        String responseMessage = ResponseMessage.RESPONSE_SIGN_UP_AVAILABLE_NICKNAME;
+        
+        given(signUpBusiness.nicknameDuplicateCheck(nickname, request.getSession().getId())).willReturn(responseMessage);
+        
+        
+        // when
+        ResponseEntity<ApiResponse<Void>> responseEntity = checkApiController.duplicateCheck(request, email, nickname);
+        
+        
+        // then
+        assertEquals(responseEntity.getStatusCode(),        HttpStatusCode.valueOf(HttpStatus.OK.value()));
+        assertEquals(responseEntity.getBody().getStatus(),  true);
+        assertEquals(responseEntity.getBody().getMessage(), responseMessage);
     }
     
     @DisplayName("닉네임 중복 확인 실패 - 규격 안맞음")
@@ -165,73 +197,170 @@ class CheckApiControllerTest {
             " 🙆🙆🙆🙆🙆🙆",
             "🙆🙆🙆 🙆🙆🙆",
     })
-    void duplicateCheck_nickname_fail(String nickname) throws Exception {
-        MockHttpSession session = new MockHttpSession();
-        
-        mvc.perform(
-                MockMvcRequestBuilders.get("/check")
-                        .param("nickname", nickname)
-                        .session(session)
-                )
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status").value(false))
-                .andExpect(jsonPath("$.message").value("입력 값이 올바르지 않습니다."))
-                .andDo(print());
-    }
-    
-    
-    @DisplayName("이메일로 인증코드 전송")
-    @Test
-    void sendEmail() throws Exception {
+    void duplicateCheck_nickname_fail(String nickname) {
         // given
-        MockHttpSession session = new MockHttpSession();
+        String email = null;
         
-        EmailAuthCodeRequest request = EmailAuthCodeRequest.of("new@email.mail");
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        String responseMessage = ExceptionMessage.EXCEPTION_VALID_NICKNAME_FORMAT;
+        
+        given(signUpBusiness.nicknameDuplicateCheck(nickname, request.getSession().getId())).willReturn(responseMessage);
         
         
         // when
-        MvcResult mvcResult = mvc.perform(
-                        MockMvcRequestBuilders.post("/check/auth")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsBytes(request))
-                                .session(session)
-                )
-                .andDo(print())
-                .andReturn();
+        ResponseEntity<ApiResponse<Void>> responseEntity = checkApiController.duplicateCheck(request, email, nickname);
         
         
         // then
-        EmailAuthCodeRequest emailAuthCodeRequest = objectMapper.readValue(
-                mvcResult.getRequest().getContentAsString(),
-                EmailAuthCodeRequest.class
+        assertEquals(responseEntity.getStatusCode(),        HttpStatusCode.valueOf(HttpStatus.OK.value()));
+        assertEquals(responseEntity.getBody().getStatus(),  false);
+        assertEquals(responseEntity.getBody().getMessage(), responseMessage);
+    }
+    
+    @DisplayName("닉네임 중복 확인 실패 - 다른 회원이 사용하고 있거나 다른 사람이 먼저 예약한 경우")
+    @Test
+    void duplicateCheck_nickname_fail_() {
+        // given
+        String nickname = "used";
+        String email = null;
+        
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        String responseMessage = ResponseMessage.RESPONSE_SIGN_UP_UNAVAILABLE_NICKNAME;
+        
+        given(signUpBusiness.nicknameDuplicateCheck(nickname, request.getSession().getId())).willReturn(responseMessage);
+        
+        
+        // when
+        ResponseEntity<ApiResponse<Void>> responseEntity = checkApiController.duplicateCheck(request, email, nickname);
+        
+        
+        // then
+        assertEquals(responseEntity.getStatusCode(),        HttpStatusCode.valueOf(HttpStatus.OK.value()));
+        assertEquals(responseEntity.getBody().getStatus(),  false);
+        assertEquals(responseEntity.getBody().getMessage(), responseMessage);
+    }
+    
+    
+    @DisplayName("이메일로 인증코드 전송 - 성공")
+    @Test
+    void sendEmail_success() {
+        // given
+        EmailAuthCodeRequest emailAuthCodeRequest = EmailAuthCodeRequest.of("new@email.mail");
+        
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        String responseMessage = ResponseMessage.RESPONSE_SEND_EMAIL_AUTH_CODE;
+        
+        given(signUpBusiness.sendEmailAuthCode(request.getSession().getId(), emailAuthCodeRequest)).willReturn(responseMessage);
+        
+        
+        // when
+        ResponseEntity<ApiResponse<Void>> responseEntity = checkApiController.sendEmail(request, emailAuthCodeRequest);
+        
+        
+        // then
+        assertEquals(responseEntity.getStatusCode(),        HttpStatusCode.valueOf(HttpStatus.OK.value()));
+        assertEquals(responseEntity.getBody().getStatus(),  true);
+        assertEquals(responseEntity.getBody().getMessage(), responseMessage);
+    }
+    
+    
+    @DisplayName("이메일로 인증코드 전송 - 실패: email 중복검사 안함")
+    @Test
+    void sendEmail_fail_cause_email_not_duplicate_check() {
+        // given
+        EmailAuthCodeRequest emailAuthCodeRequest = EmailAuthCodeRequest.of("new@email.mail");
+        
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        String responseMessage = ExceptionMessage.EXCEPTION_REQUIRE_MAIL_DUPLICATE_CHECK;
+        
+        given(signUpBusiness.sendEmailAuthCode(request.getSession().getId(), emailAuthCodeRequest)).willThrow(new ApiException(responseMessage));
+        
+        
+        // when - then
+        assertThrows(
+                ApiException.class,
+                () -> checkApiController.sendEmail(request, emailAuthCodeRequest),
+                responseMessage
         );
-        
-        assertEquals(emailAuthCodeRequest, request);
     }
     
-    @DisplayName("메일 인증코드 검증")
+    @DisplayName("이메일로 인증코드 전송 - 실패: java mail sender error")
     @Test
-    void authEmailCode() throws Exception {
+    void sendEmail_fail_cause_java_mail_sender() {
         // given
-        MockHttpSession session = new MockHttpSession();
+        EmailAuthCodeRequest emailAuthCodeRequest = EmailAuthCodeRequest.of("new@email.mail");
         
-        String requestCode = "auth-code";
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        String responseMessage = ExceptionMessage.EXCEPTION_MAIL_SEND_FAIL;
+        
+        given(signUpBusiness.sendEmailAuthCode(request.getSession().getId(), emailAuthCodeRequest)).willThrow(new RuntimeException(responseMessage));
+        
+        
+        // when - then
+        assertThrows(
+                RuntimeException.class,
+                () -> checkApiController.sendEmail(request, emailAuthCodeRequest),
+                responseMessage
+        );
+    }
+    
+    
+    @DisplayName("메일 인증코드 검증 - 성공")
+    @Test
+    void authEmailCode_success() {
+        // given
+        String code = "auth-code";
+        
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        String responseMessage = ResponseMessage.RESPONSE_VERIFY_EMAIL_AUTH_SUCCESS;
+        
+        given(signUpBusiness.verifyEmailAuthCode(request.getSession().getId(), code)).willReturn(responseMessage);
         
         
         // when
-        MvcResult mvcResult = mvc.perform(
-                        MockMvcRequestBuilders.get("/check/auth")
-                                .param("code", requestCode)
-                                .session(session)
-                )
-                .andDo(print())
-                .andReturn();
+        ResponseEntity<ApiResponse<Void>> responseEntity = checkApiController.authEmailCode(request, code);
         
         
         // then
-        String readCode = mvcResult.getRequest().getParameterValues("code")[0];
+        assertEquals(responseEntity.getStatusCode(),        HttpStatusCode.valueOf(HttpStatus.OK.value()));
+        assertEquals(responseEntity.getBody().getStatus(),  true);
+        assertEquals(responseEntity.getBody().getMessage(), responseMessage);
+    }
+    
+    @DisplayName("메일 인증코드 검증 - 실패: 인증 코드 만료 혹은 인증 코드 불일치")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            ExceptionMessage.EXCEPTION_VERIFY_EMAIL_AUTH_CODE_EXPIRED,
+            ExceptionMessage.EXCEPTION_VERIFY_FAIL_EMAIL_AUTH_CODE
+    })
+    void authEmailCode_fail(String responseMessage) {
+        // given
+        String code = "auth-code";
         
-        assertEquals(readCode, requestCode);
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        given(signUpBusiness.verifyEmailAuthCode(request.getSession().getId(), code)).willThrow(new ApiException(responseMessage));
+        
+        
+        // when - then
+        assertThrows(
+                ApiException.class,
+                () -> checkApiController.authEmailCode(request, code),
+                responseMessage
+        );
     }
     
 }
