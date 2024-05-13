4df063539b6f249a1797e193e8042427339bf880
zhyunk <gimwlgus@gmail.com>
2024-05-13 23:00:42 +0900

    test:  [user]  sign api controller test 재작성
    webMvc -> mock
    
    issue key : #81

--- simpleBoard02/b-server-user/src/test/java/kim/zhyun/serveruser/domain/signup/controller/SignApiControllerTest.java
+++ simpleBoard02/b-server-user/src/test/java/kim/zhyun/serveruser/domain/signup/controller/SignApiControllerTest.java
@@ -1,57 +1,88 @@
 package kim.zhyun.serveruser.domain.signup.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import kim.zhyun.serveruser.container.RedisTestContainer;
+import kim.zhyun.jwt.common.model.ApiResponse;
+import kim.zhyun.jwt.exception.ApiException;
+import kim.zhyun.serveruser.common.message.ExceptionMessage;
+import kim.zhyun.serveruser.domain.signup.business.SignUpBusiness;
 import kim.zhyun.serveruser.domain.signup.controller.model.SignupRequest;
+import org.apache.hc.core5.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
 
+import static kim.zhyun.serveruser.common.message.ResponseMessage.RESPONSE_SUCCESS_FORMAT_SIGN_UP;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 
-@Order(0)
-@ExtendWith(RedisTestContainer.class)
-@AutoConfigureMockMvc
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class SignApiControllerTest {
     
-    @Autowired MockMvc mvc;
-    @Autowired ObjectMapper objectMapper;
+    @InjectMocks SignApiController signApiController;
+    @Mock SignUpBusiness signUpBusiness;
     
     
     
-    @DisplayName("회원가입")
+    @DisplayName("회원가입 - 성공")
     @Test
-    void signUp() throws Exception {
+    void signUp_success() {
         // given
         SignupRequest signupRequest = SignupRequest.of(
                 "new@email.mail", "닉네임", "password"
         );
         
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        String responseMessage = RESPONSE_SUCCESS_FORMAT_SIGN_UP;
+        
+        given(signUpBusiness.saveMember(request.getSession().getId(), signupRequest)).willReturn(responseMessage);
+        
+        
         // when
-        MvcResult mvcResult = mvc.perform(
-                        post("/sign-up")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsBytes(signupRequest))
-                )
-                .andDo(print())
-                .andReturn();
+        ResponseEntity<ApiResponse<Void>> responseEntity = signApiController.signUp(request, signupRequest);
+        
         
         // then
-        SignupRequest readBody = objectMapper.readValue(
-                mvcResult.getRequest().getContentAsString(),
-                SignupRequest.class
+        assertEquals(responseEntity.getStatusCode(), HttpStatusCode.valueOf(HttpStatus.SC_OK));
+        assertEquals(responseEntity.getBody().getStatus(), true);
+        assertEquals(responseEntity.getBody().getMessage(), responseMessage);
+    }
+    
+    
+    @DisplayName("회원가입 - 실패: email 혹은 nickname 중복확인 안함")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            ExceptionMessage.EXCEPTION_REQUIRE_MAIL_DUPLICATE_CHECK,
+            ExceptionMessage.EXCEPTION_REQUIRE_NICKNAME_DUPLICATE_CHECK
+    })
+    void signUp_fail(String responseMessage) {
+        // given
+        SignupRequest signupRequest = SignupRequest.of(
+                "new@email.mail", "닉네임", "password"
         );
-        assertEquals(readBody, signupRequest);
+        
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
+        
+        given(signUpBusiness.saveMember(request.getSession().getId(), signupRequest)).willThrow(new ApiException(responseMessage));
+        
+        
+        // when - then
+        assertThrows(
+                ApiException.class,
+                () -> signApiController.signUp(request, signupRequest),
+                responseMessage
+        );
     }
     
 }
