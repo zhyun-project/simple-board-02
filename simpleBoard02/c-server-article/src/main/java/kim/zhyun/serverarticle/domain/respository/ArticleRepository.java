383fe45d25dbb7759b40e52f8097686e421597e6
zhyunk <gimwlgus@gmail.com>
2024-05-14 00:14:24 +0900

    fix:  [article] article repository 구현 할 인터페이스 변경
    JpaRepostory -> Repository
    
    + 추가 : deleteAllInBatch에서 받는 매개변수를 Collection<T>가 아니라 Iterable<T>로 했어야 하는 거였다!!
    
    issue key : #82

--- simpleBoard02/c-server-article/src/main/java/kim/zhyun/serverarticle/domain/respository/ArticleRepository.java
+++ simpleBoard02/c-server-article/src/main/java/kim/zhyun/serverarticle/domain/respository/ArticleRepository.java
@@ -1,13 +1,20 @@
 package kim.zhyun.serverarticle.domain.respository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
+public interface ArticleRepository extends Repository<ArticleEntity, Long> {
     
     List<ArticleEntity> findAllByUserIdOrderByCreatedAtDesc(long userId);
     Optional<ArticleEntity> findByUserIdAndArticleId(long userId, long articleId);
     
+    List<ArticleEntity> findAll(Sort createdAt);
+    
+    ArticleEntity save(ArticleEntity entity);
+    
+    void deleteAllInBatch(Iterable<ArticleEntity> entities);
+
 }
