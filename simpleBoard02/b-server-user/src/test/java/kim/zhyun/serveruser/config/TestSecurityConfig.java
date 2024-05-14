c697528115cea14a0d22db85d6f4b761195fb797
zhyunk <gimwlgus@gmail.com>
2024-05-15 02:59:22 +0900

    test:  [user]  security 설정 추가
    issue key : #81

---
+++ simpleBoard02/b-server-user/src/test/java/kim/zhyun/serveruser/config/TestSecurityConfig.java
@@ -0,0 +1,71 @@
+package kim.zhyun.serveruser.config;
+
+
+import kim.zhyun.jwt.exception.ApiException;
+import kim.zhyun.jwt.filter.ExceptionHandlerFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
+
+import static kim.zhyun.jwt.common.constants.type.RoleType.TYPE_ADMIN;
+import static kim.zhyun.jwt.common.constants.type.RoleType.TYPE_MEMBER;
+import static kim.zhyun.jwt.exception.message.CommonExceptionMessage.EXCEPTION_AUTHENTICATION;
+import static kim.zhyun.jwt.exception.message.CommonExceptionMessage.EXCEPTION_PERMISSION;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@RequiredArgsConstructor
+@EnableWebSecurity(debug = true)
+@EnableMethodSecurity(prePostEnabled = true)
+@TestConfiguration
+public class TestSecurityConfig {
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+    
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        
+        http.authorizeHttpRequests(config -> config
+                .requestMatchers(
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        
+                        "/error/**",
+                        
+                        "/sign-up/**",
+                        "/check/**",
+                        "/login/**"
+                ).permitAll()
+                
+                .requestMatchers(
+                        "/withdrawal/**"
+                ).authenticated()
+                
+                .anyRequest().hasAnyRole(TYPE_ADMIN, TYPE_MEMBER));
+        
+        http.exceptionHandling(config -> config
+                .accessDeniedHandler((request, response, exception) -> {
+                    // 접근할 수 없는 권한
+                    throw new ApiException(EXCEPTION_PERMISSION);
+                })
+                .authenticationEntryPoint((request, response, exception) -> {
+                    // 유효한 자격증명을 제공하지 않고 접근하려 할때
+                    throw new ApiException(EXCEPTION_AUTHENTICATION);
+                }));
+        
+        http.httpBasic(withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(AbstractHttpConfigurer::disable);
+        http.logout(AbstractHttpConfigurer::disable);
+        
+        http.addFilterBefore(exceptionHandlerFilter, SecurityContextHolderFilter.class);
+        
+        return http.build();
+    }
+
+}
