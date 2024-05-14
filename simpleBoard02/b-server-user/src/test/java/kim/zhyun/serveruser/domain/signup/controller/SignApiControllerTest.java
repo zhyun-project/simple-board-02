37d3cad7dfe82805e5ad76763bed68d37cbb0912
zhyunk <gimwlgus@gmail.com>
2024-05-15 03:04:56 +0900

    test:  [user] sign api controller test 재작성
    재작성 2회차
    
    issue key : #81

--- simpleBoard02/b-server-user/src/test/java/kim/zhyun/serveruser/domain/signup/controller/SignApiControllerTest.java
+++ simpleBoard02/b-server-user/src/test/java/kim/zhyun/serveruser/domain/signup/controller/SignApiControllerTest.java
@@ -1,61 +1,81 @@
 package kim.zhyun.serveruser.domain.signup.controller;
 
-import kim.zhyun.jwt.common.model.ApiResponse;
 import kim.zhyun.jwt.exception.ApiException;
+import kim.zhyun.jwt.filter.JwtFilter;
 import kim.zhyun.serveruser.common.message.ExceptionMessage;
+import kim.zhyun.serveruser.config.TestSecurityConfig;
 import kim.zhyun.serveruser.domain.signup.business.SignUpBusiness;
 import kim.zhyun.serveruser.domain.signup.controller.model.SignupRequest;
-import org.apache.hc.core5.http.HttpStatus;
+import kim.zhyun.serveruser.filter.AuthenticationFilter;
+import kim.zhyun.serveruser.filter.SessionCheckFilter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import static kim.zhyun.serveruser.common.message.ResponseMessage.RESPONSE_SUCCESS_FORMAT_SIGN_UP;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@Import(TestSecurityConfig.class)
+@WebMvcTest(
+        controllers = SignApiController.class,
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = {
+                        AuthenticationFilter.class,
+                        JwtFilter.class,
+                        SessionCheckFilter.class
+                }
+        )
+)
 class SignApiControllerTest {
     
-    @InjectMocks SignApiController signApiController;
-    @Mock SignUpBusiness signUpBusiness;
+    @MockBean SignUpBusiness signUpBusiness;
     
+    @Autowired MockMvc mvc;
+    ObjectMapper objectMapper = new ObjectMapper();
     
     
     @DisplayName("회원가입 - 성공")
     @Test
-    void signUp_success() {
+    void signUp_success() throws Exception {
         // given
         SignupRequest signupRequest = SignupRequest.of(
                 "new@email.mail", "닉네임", "password"
         );
         
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setSession(new MockHttpSession());
+        MockHttpSession session = new MockHttpSession();
         
         String responseMessage = RESPONSE_SUCCESS_FORMAT_SIGN_UP;
         
-        given(signUpBusiness.saveMember(request.getSession().getId(), signupRequest)).willReturn(responseMessage);
+        given(signUpBusiness.saveMember(session.getId(), signupRequest)).willReturn(responseMessage);
         
         
-        // when
-        ResponseEntity<ApiResponse<Void>> responseEntity = signApiController.signUp(request, signupRequest);
-        
-        
-        // then
-        assertEquals(responseEntity.getStatusCode(), HttpStatusCode.valueOf(HttpStatus.SC_OK));
-        assertEquals(responseEntity.getBody().getStatus(), true);
-        assertEquals(responseEntity.getBody().getMessage(), responseMessage);
+        // when - then
+        mvc.perform(
+                post("/sign-up")
+                        .session(session)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(signupRequest))
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(true))
+                .andExpect(jsonPath("$.message").value(responseMessage))
+                .andDo(print());
     }
     
     
@@ -65,24 +85,28 @@ class SignApiControllerTest {
             ExceptionMessage.EXCEPTION_REQUIRE_MAIL_DUPLICATE_CHECK,
             ExceptionMessage.EXCEPTION_REQUIRE_NICKNAME_DUPLICATE_CHECK
     })
-    void signUp_fail(String responseMessage) {
+    void signUp_fail(String responseMessage) throws Exception {
         // given
         SignupRequest signupRequest = SignupRequest.of(
                 "new@email.mail", "닉네임", "password"
         );
         
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setSession(new MockHttpSession());
+        MockHttpSession session = new MockHttpSession();
         
-        given(signUpBusiness.saveMember(request.getSession().getId(), signupRequest)).willThrow(new ApiException(responseMessage));
+        given(signUpBusiness.saveMember(session.getId(), signupRequest)).willThrow(new ApiException(responseMessage));
         
         
         // when - then
-        assertThrows(
-                ApiException.class,
-                () -> signApiController.signUp(request, signupRequest),
-                responseMessage
-        );
+        mvc.perform(
+                        post("/sign-up")
+                                .session(session)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsBytes(signupRequest))
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(false))
+                .andExpect(jsonPath("$.message").value(responseMessage))
+                .andDo(print());
     }
     
 }
