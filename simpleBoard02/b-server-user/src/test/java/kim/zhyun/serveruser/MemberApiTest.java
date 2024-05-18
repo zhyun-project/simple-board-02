8ab8afddc93aa7c0a7779c26ade2d5aea740874f
zhyunk <gimwlgus@gmail.com>
2024-05-18 19:45:19 +0900

    test:  [user]  authentication filter test, member api test
    1. member api test- login 실행 후 status 확인 추가
    2. authentication filter test 추가
    
    issue key : #81

--- simpleBoard02/b-server-user/src/test/java/kim/zhyun/serveruser/MemberApiTest.java
+++ simpleBoard02/b-server-user/src/test/java/kim/zhyun/serveruser/MemberApiTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Disabled("전체 테스트 실행시 무시되도록 설정 켜야 됨")
+//@Disabled("전체 테스트 실행시 무시되도록 설정 켜야 됨")
 @AutoConfigureMockMvc
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -46,6 +46,8 @@ public class MemberApiTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(userInfo))
                 )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(true))
                 .andDo(print());
     }
     static Stream<Map<String, String>> login() {
