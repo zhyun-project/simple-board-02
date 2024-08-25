f24d698bbd629bf108f31dada368cd41cf928a1a
zhyunk <gimwlgus@gmail.com>
2024-08-25 19:40:01 +0900

    🛠️ fix: [user-server]  resources/*.sql  -  ROLE 테이블 관련 쿼리 수정
    - id 값 직접 입력하도록 변경
    - 서버 실행시 ROLE 테이블 삭제 후 생성하도록 변경

--- simpleBoard02/b-server-user/src/main/resources/data.sql
+++ simpleBoard02/b-server-user/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 -- Role Data
-INSERT INTO "ROLE" (grade, description) VALUES ( 'ADMIN', '관리자' );
-INSERT INTO "ROLE" (grade, description) VALUES ( 'MEMBER', '회원' );
-INSERT INTO "ROLE" (grade, description) VALUES ( 'WITHDRAWAL', '탈퇴' );
+INSERT INTO "ROLE" (id, grade, description) VALUES (1, 'ADMIN', '관리자' );
+INSERT INTO "ROLE" (id, grade, description) VALUES (2, 'MEMBER', '회원' );
+INSERT INTO "ROLE" (id, grade, description) VALUES (3, 'WITHDRAWAL', '탈퇴' );
