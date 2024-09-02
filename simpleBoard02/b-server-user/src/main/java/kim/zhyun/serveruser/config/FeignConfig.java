6740dd3d72d99f0214efe5cc6044655c1a31fa72
zhyunk <gimwlgus@gmail.com>
2024-09-02 21:30:45 +0900

    🛠️: feign client 설정 추가

--- simpleBoard02/b-server-user/src/main/java/kim/zhyun/serveruser/config/FeignConfig.java
+++ simpleBoard02/b-server-user/src/main/java/kim/zhyun/serveruser/config/FeignConfig.java
@@ -1,6 +1,8 @@
 package kim.zhyun.serveruser.config;
 
 import feign.Logger;
+import feign.Request;
+import feign.Retryer;
 import feign.form.spring.SpringFormEncoder;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
@@ -8,12 +10,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class FeignConfig {
     
     @Bean
     public SpringFormEncoder multipartFormEncoder() {
         return new SpringFormEncoder(new SpringEncoder(() -> new HttpMessageConverters(new RestTemplate().getMessageConverters())));
+    }
+
+    // 타임아웃 설정
+    @Bean
+    public Request.Options feignOptions() {
+        return new Request.Options(Duration.ofSeconds(1), Duration.ofSeconds(5), false);
+    }
+
+    // 재시도 설정
+    @Bean
+    public Retryer feignRetryer() {
+        return new Retryer.Default(1000, 5000, 10);
     }
     
     @Bean
--- simpleBoard02/b-server-user/src/main/java/kim/zhyun/serveruser/domain/member/client/ArticleClient.java
+++ simpleBoard02/b-server-user/src/main/java/kim/zhyun/serveruser/domain/member/client/ArticleClient.java
@@ -1,6 +1,7 @@
 package kim.zhyun.serveruser.domain.member.client;
 
 import kim.zhyun.jwt.common.model.ApiResponse;
+import kim.zhyun.serveruser.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Set;
 
-@FeignClient(name = "articleClient", url = "${client.server}/api/article")
+@FeignClient(name = "articleClient", url = "${client.server}/api/article", configuration = FeignConfig.class)
 public interface ArticleClient {
     
     @PostMapping("/delete/withdrawal")
